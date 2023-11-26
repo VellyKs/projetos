048491457ad1a05fab1e8187d9b930bc2cbb0253
Evellyn KS <132318866+VellyKs@users.noreply.github.com>
2023-11-26 07:57:27 -0300

    iniciando um novo portfolio

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-m
+'use strict'
+//projetos nav
+
+const navegar = (target) => {
+    const pai = document.getElementById('titulos')
+    const old = pai.querySelector('.active')
+    old.classList.remove('active')
+    const element = target.target;
+    console.log(old)
+    
+    element.classList.add('active')
+
+    
+
+}
+
+document.getElementById('titulos').addEventListener('click', navegar);
