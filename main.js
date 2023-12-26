5ea3a339e3c5a558d0216a141e84b817fff6582d
evellyn karla <evellynkarla1418@gmail.com>
2023-12-26 17:51:10 -0300

    Atualizando

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 //projetos nav
 
@@ -15,22 +14,4 @@ const navegar = (target) => {
 
 }
 
-=======
-'use strict'
-//projetos nav
-
-const navegar = (target) => {
-    const pai = document.getElementById('titulos')
-    const old = pai.querySelector('.active')
-    old.classList.remove('active')
-    const element = target.target;
-    console.log(old)
-    
-    element.classList.add('active')
-
-    
-
-}
-
->>>>>>> 4fd211a4143bf293297aa8114ac37cfd6b46139f
 document.getElementById('titulos').addEventListener('click', navegar);
